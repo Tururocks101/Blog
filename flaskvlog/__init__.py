7d11903d32b90efe31c772806e439b1d5e18eaa4
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-03-14 08:34:24 +0300

    Add files via upload

--- flaskvlog/__init__.py
+++ flaskvlog/__init__.py
@@ -1,10 +1,11 @@
 
-
+import os
 from flaskvlog import routes
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -15,3 +16,9 @@ bcrypt = Bcrypt(app)
 login_manager = Login_manager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
